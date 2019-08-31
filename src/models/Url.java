682e54808dd3544b466743d4ea45371cea884d9f
Italo Guimarães <italo.guimaraes1994@gmail.com>
2019-08-31 09:32:46 -0300

    Adicinando depedencias e criando estrutura inicial.

---
+++ src/models/Url.java
@@ -0,0 +1,27 @@
+package models;
+
+public class Url {
+	
+	private String url;
+	private String imageUrl;
+	private String textUrl;
+	
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public String getTextUrl() {
+		return textUrl;
+	}
+	public void setTextUrl(String textUrl) {
+		this.textUrl = textUrl;
+	}
+}
